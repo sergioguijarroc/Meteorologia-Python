cc488a41344d36413f1e6fff7a6aa67587f56519
sergioguijarroc <sergioguijarroc@gmail.com>
2023-10-10 21:41:19 +0200

    Menú interactivo hecho

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import openai
 import json
 
 
@@ -18,45 +17,86 @@ try:
     if response.status_code == 200:
         datosClima = response.json()
 
-        tempActual = datosClima["main"]["temp"]
-        tempMax = datosClima["main"]["temp_max"]
-        tempMin = datosClima["main"]["temp_min"]
-
-        openai.api_key = "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"
-        completion = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=f"Este es el tiempo en {provincia}, la temperatura actual es de {tempActual}ºC, la temperatura máxima es de {tempMax}ºC y la temperatura mínima es de {tempMin}ºC. ¿Podrías hacerme un resumen y un comentario gracioso acerca estos datos?",
-            max_tokens=2048,
-        )
-
         # prompt = f"Los datos del pronóstico del tiempo en {provincia} son los siguientes:\n\n- Temperatura actual: {tempActual}°C\n- Temperatura máxima: {tempMax}°C\n- Temperatura mínima: {tempMin}°C\n- Sensación térmica: {sensTermica}°C\n- Velocidad del viento: {velViento} m/s\n- Nivel de nubes: {nubes}%\n\n ¿Qué conclusiones podemos sacar de esta información? ¿Cómo afectará esta temperatura a las actividades al aire libre en {provincia}? ¿Algún consejo basado en estos datos para los residentes?"
 
-        print(completion.choices[0].text)
+        # Menú
+        print("--------------------Menu--------------------")
+        print("1. Temperatura actual")
+        print("2. Temperatura máxima")
+        print("3. Temperatura mínima")
+        print("4. Sensación térmica")
+        print("5. Velocidad del viento")
+        print("6. Nivel de nubes")
+        print("7. Salir")
+        print("--------------------------------------------")
 
-        # Imprimo la información
-        print(
-            f"La temperatura actual en {provincia} es de : {tempActual} ºC, la máxima es de {tempMax} ºC y  la mínima es de {tempMin} ºC"
-        )
+        # Pregunto al usuario que quiere saber
+    # Realiza la solicitud GET a la URL para comprobar que comunica.
+    response = requests.get(url)
 
-        respuesta = input("¿Quieres ampliar información? (Responde con sí o no )")
-        while respuesta != "si" and respuesta != "no":
-            respuesta = input("Respuesta errónea, debes responder con sí o no ")
-        if respuesta == "si":
-            sensTermica = datosClima["main"]["feels_like"]
-            velViento = datosClima["wind"]["speed"]
-            nubes = datosClima["clouds"]["all"]
-            nubosidad = ""
-            if nubes == 0:
-                nubosidad = "despejado de nubes"
-            if nubes > 0 < 50:
-                nubosidad = "algo nublado"
-            if nubes > 50:
-                nubosidad = "muy nublado"
+    # Si la solicitud se hace con éxito, el código de la respuesta devuelve 200.
+    if response.status_code == 200:
+        datosClima = response.json()
+
+        # Menú
+        print("--------------------Menu--------------------")
+        print("1. Temperatura actual")
+        print("2. Temperatura máxima")
+        print("3. Temperatura mínima")
+        print("4. Sensación térmica")
+        print("5. Velocidad del viento")
+        print("6. Nivel de nubes")
+        print("7. Salir")
+        print("--------------------------------------------")
+
+        # Pregunto al usuario que quiere saber
+        salir = False
+        while not salir:
+            opcion = int(input("Elige una opción: "))
+            if opcion in range(1, 8):
+                if opcion == 1:
+                    tempActual = datosClima["main"]["temp"]
+                    print(
+                        f"La temperatura actual en {provincia} es de : {tempActual} ºC"
+                    )
+                elif opcion == 2:
+                    tempMax = datosClima["main"]["temp_max"]
+                    print(f"La temperatura máxima en {provincia} es de : {tempMax} ºC")
+                elif opcion == 3:
+                    tempMin = datosClima["main"]["temp_min"]
+                    print(f"La temperatura mínima en {provincia} es de : {tempMin} ºC")
+                elif opcion == 4:
+                    sensTermica = datosClima["main"]["feels_like"]
+                    print(
+                        f"La sensación térmica en {provincia} es de : {sensTermica} ºC"
+                    )
+                elif opcion == 5:
+                    velViento = datosClima["wind"]["speed"]
+                    print(
+                        f"La velocidad del viento en {provincia} es de : {velViento} KM/h"
+                    )
+                elif opcion == 6:
+                    nubes = datosClima["clouds"]["all"]
+                    if 0 < nubes < 50:
+                        nubosidad = "algo nublado"
+                    elif nubes >= 50:
+                        nubosidad = "muy nublado"
+                    else:
+                        nubosidad = "despejado de nubes"
+                    print(
+                        f"El nivel de nubes en {provincia} es de : {nubes} %, por lo tanto está {nubosidad}"
+                    )
+                elif opcion == 7:
+                    print("¡Que tenga un buen día!")
+                    salir = True
+            else:
+                print("Opción incorrecta, elige una opción del 1 al 7.")
+
+        # Si hay un error en la petición, devuelve esto:
+        else:
             print(
-                f"De acuerdo, aquí tienes la velocidad del viento, {velViento} KM/h, la sensación térmica es de {sensTermica} ºC, y el cielo está {nubosidad}"
+                f"Error al realizar la solicitud. Código de estado: {response.status_code}"
             )
-        elif respuesta == "no":
-            print("Usted se lo pierde, ¡Que tenga un buen día!")
 
     # Si hay un error en la petición, devuelve esto:
     else:
