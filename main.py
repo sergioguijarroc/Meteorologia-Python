0c91772b1d0934c7cf59e3b0d5313b079fdd81d8
sergioguijarroc <sergioguijarroc@gmail.com>
2023-10-10 10:01:44 +0200

    Programa 100% Funcional pero básico
    Te da las temperaturas, te pregunta si quieres más información y te ofrece la sensación térmica, el viento y la nubosidad

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
-# Solicitar el nombre de la provincia en inglés
-provincia = input("Dime la provincia en inglés que quieres consultar: ")
+
+print("Bienvenido a tu meteorólogo de confianza, selecciona una opción para ")
+# Solicitar el nombre de la provincia
+provincia = input("Dime la provincia que quieres consultar: ")
 
 # URL de la API de pronóstico del tiempo (sustituye por la URL correcta)
 url = f"https://api.openweathermap.org/data/2.5/weather?q={provincia}&units=metric&appid=ddccd66e42b270c765e7ea196e4e220c"
@@ -21,10 +23,30 @@ try:
 
         # Imprimo la información
         print(
-            f"La temperatura actual en la provincia de {provincia} es de : {tempActual}"
+            f"La temperatura actual en {provincia} es de : {tempActual} ºC, la máxima es de {tempMax} ºC y  la mínima es de {tempMin} ºC"
         )
-        print(f"La temperatura máxima en la provincia de {provincia} es de : {tempMax}")
-        print(f"La temperatura mínima en la provincia de {provincia} es de : {tempMin}")
+
+        respuesta = input("¿Quieres ampliar información? (Responde con sí o no )")
+        while respuesta != "si" and respuesta != "no":
+            respuesta = input("Respuesta errónea, debes responder con sí o no ")
+        if respuesta == "si":
+            sensTermica = datosClima["main"]["feels_like"]
+            velViento = datosClima["wind"]["speed"]
+            nubes = datosClima["clouds"]["all"]
+            nubosidad = ""
+            if nubes == 0:
+                nubosidad = "despejado de nubes"
+            if nubes > 0 < 50:
+                nubosidad = "algo nublado"
+            if nubes > 50:
+                nubosidad = "muy nublado"
+            print(
+                f"De acuerdo, aquí tienes la velocidad del viento, {velViento} KM/h, la sensación térmica es de {sensTermica} ºC, y el cielo está {nubosidad}"
+            )
+        elif respuesta == "no":
+            print("Usted se lo pierde, ¡Que tenga un buen día!")
+        else:
+            print("Error de la respuesta, dígame otra")
 
     # Si hay un error en la petición, devuelve esto:
     else:
